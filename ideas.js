744a3a193969d726754da425c79a3a5640d53a12
Ayushman Swain <ayushmanswain2006@gmail.com>
2025-07-13 19:17:35 +0530

    Add files via upload

---
+++ ideas.js
@@ -0,0 +1,33 @@
+const ideas = [
+  {
+    name: "AI Tutor",
+    pitch: "An AI that teaches students based on their pace.",
+    category: "EdTech",
+    description: "AI Tutor is an adaptive platform using ML to personalize lessons and assessments. It adjusts content based on student performance in real time.",
+    email: "founder@aitutor.com",
+    website: "https://aitutor.com"
+  },
+  {
+    name: "Green Society",
+    pitch: "Social reform for environmental action.",
+    category: "Social",
+    description: "A platform to track, reward, and encourage eco-friendly behavior using challenges and reporting tools.",
+    email: "eco@greensociety.org",
+    website: ""
+  },
+  {
+    name: "CodeConnect",
+    pitch: "A networking app for coders.",
+    category: "Tech",
+    description: "Connect student developers with real-world projects, mentors, and peers through one platform.",
+    email: "team@codeconnect.dev",
+    website: "https://codeconnect.dev"
+  },
+  {
+  name: "Ecycle India",
+  pitch: "India’s first circular e-waste marketplace.",
+  category: "Social",
+  description: "Ecycle India connects businesses and consumers with certified e-waste collectors and upcyclers. It enables listing of used electronics, bidding by recyclers, and digital tracking of collection. Helps industries meet e-waste compliance goals.",
+  email: "eco@ecycle.org"
+}
+];
---
+++ script.js
@@ -0,0 +1,137 @@
+//Scrolling Feature
+const navBar = document.querySelector(".nav-bar");
+
+const scrollTop = document.querySelector(".scroll-top");
+
+//Nav Bar Background
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    navBar.classList.add("scrolled");
+  } else {
+    navBar.classList.remove("scrolled");
+  }
+
+  //Scroll to Top
+  scrollTop.style.display = window.scrollY > 500 ? "flex" : "none";
+});
+
+//Scroll to Top behaviour
+scrollTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+//Logo click
+const logo = document.querySelector(".logo");
+
+if (logo) {
+  logo.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+}
+
+const footerLogo = document.querySelector(".footer-logo");
+
+if (footerLogo) {
+  footerLogo.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+}
+
+//Start-Up Ideas
+ideas.forEach((idea, index) => {
+  idea.id = index;
+});
+function displayIdeaCards(data) {
+  const cardsSet = document.querySelector(".cards-set");
+
+  cardsSet.innerHTML = "";
+
+  data.forEach((idea) => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+      <h3>${idea.name}</h3>
+      <p class="pitch"><b>Pitch:</b> ${idea.pitch}</p>
+      <p class="idea-description">${idea.description.slice(0, 100)}...</p>
+      <p class="category"><b>Category:</b> <span class="badge-card">${
+        idea.category
+      }</span></p>
+      <a href="ideas.html?id=${idea.id}"><span class="more-btn">+</span></a>
+    `;
+
+    cardsSet.appendChild(card);
+  });
+}
+
+
+displayIdeaCards(ideas);
+
+//Search and Filter
+const searchInput = document.querySelector(".search");
+
+const categoryFilter = document.querySelector(".categoryFilter");
+
+function applyFilters() {
+  const searchTerm = searchInput.value.toLowerCase();
+  const selectedCategory = categoryFilter.value;
+
+  const filterIdeas = ideas.filter((idea) => {
+    const matchSearch =
+      idea.name.toLowerCase().includes(searchTerm) ||
+      idea.pitch.toLowerCase().includes(searchTerm);
+
+    const matchCategory =
+      selectedCategory === "All" || idea.category === selectedCategory;
+
+    return matchSearch && matchCategory;
+  });
+
+  displayIdeaCards(filterIdeas);
+}
+
+if (searchInput && categoryFilter) {
+  searchInput.addEventListener("input", applyFilters);
+  categoryFilter.addEventListener("change", applyFilters);
+}
+
+//Form submit button
+const form = document.querySelector(".form-container");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  alert("Your form is submitted! Thank you for your idea.");
+  form.reset();
+
+  setTimeout(() => {
+    location.href="#form";
+  },500);
+});
+
+//Nav Bar Toggle
+const toggle = document.querySelector(".toggle");
+const naviBar = document.querySelector(".nav-bar");
+const navList = document.querySelectorAll(".nav-list li a");
+const navLogo = document.querySelector(".logo");
+
+toggle.addEventListener("click", () => {
+  naviBar.classList.add("open");
+});
+navList.forEach(list => {
+  list.addEventListener("click", () => {
+    naviBar.classList.remove("open");
+  });
+});
+navLogo.addEventListener("click", () => {
+  naviBar.classList.remove("open");
+});
+
